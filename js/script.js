168cbf516e5b0fc2e01e0737938a82ffff37abda
EA Nugroho <69884456+JourneyEAN@users.noreply.github.com>
2024-05-12 00:20:07 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+    
+    var typed = new Typed(".typing", {
+        strings:[" ٱلسَّلَامُ عَلَيْكُمْ ","SALAM SAMIRA", "MULIA", "BAHAGIA","SEJAHTERA","INSPIRASI UMROHKU","Labaik Labaik Allahuma Labaik"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    
+    var typed = new Typed(".typing-2", {
+        strings: [" ٱلسَّلَامُ عَلَيْكُمْ ","SALAM SAMIRA", "MULIA", "BAHAGIA","SEJAHTERA","INSPIRASI UMROHKU","Labaik Labaik Allahuma Labaik"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+   
+
+});
